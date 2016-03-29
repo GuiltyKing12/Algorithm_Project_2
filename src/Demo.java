a75b27b2b66c2559f3ef5fb6b71626fbcb39a174
GuiltyKing12 <bher@mines.edu>
2016-03-29 08:54:15 -0600

    added more stuff

--- src/Demo.java
+++ src/Demo.java
@@ -4,7 +4,6 @@ public class Demo {
 	Cell[][] cost_table = new Cell[n + 1][L + 1]; // 
 	int P = 5;
 	int c = 2;
-	int current_L = 0;
 	int[] gas_amounts = {4, 2, 3, 5, 7, 9 ,2, 6};
 	int C = 1;
 	
@@ -13,23 +12,30 @@ public class Demo {
 		cost_table[0][0] = new Cell();
 		cost_table[0][0].cost = 0;
 				
-		for(int i = 0; i < L; i ++) { 
+		for(int i = 0; i <= L; i ++) { 
 					cost_table[0][i] = new Cell();
 					cost_table[0][i].cost = 999999;
 		}
 
-		for(int j = 1; j < n; j ++) {
-			for(int k = 0; k < L; k++) {
+		for(int j = 1; j <= n; j ++) {
+			System.out.print("Day " + j + ": ");
+			for(int k = 0; k <= L; k++) {
+				System.out.print(" " + k + " ");
 				cost_table[j][k] = new Cell();
 				cost_table[j][k].order = false;
 				cost_table[j][k].cost = 999999;
-				for(int X = max(0, k + gas_amounts[j] - L); X < k + gas_amounts[j]; X++) {
-					int temp_cost = order(X) + C*(X) + cost_table[j -1][k + gas_amounts[j] - X].cost + c * (k + gas_amounts[j] - X);
+				int day = 0;
+				for(int X = max(0, k + gas_amounts[day] - L); X <= k + gas_amounts[day]; X++) {
+					System.out.print("To " + (gas_amounts[day] + k) + " ");
+					int temp_cost = order(X) + C*(X) + cost_table[j - 1][k + gas_amounts[day] - X].cost + c * (k + gas_amounts[day] - X);
 					if(temp_cost < cost_table[j][k].cost) {
 						cost_table[j][k].cost = temp_cost;
-							cost_table[j][k].order = true;
+						cost_table[j][k].order = true;
+						System.out.println("here");
 					}
 				}
+				System.out.println();
+				day++;
 			}
 		}
 	}
@@ -49,12 +55,11 @@ public class Demo {
 	public static void main(String[] args) {
 		Demo demo = new Demo();
 		demo.cost_calculate(demo.gas_amounts);
-		int j = demo.L;
-		for(int i = demo.n; i > 0; i++) {
-			if(demo.cost_table[i][j].order) {
-				System.out.println("Ordered on Day: " + i);
+		for(int i = 0; i <= demo.n; i++) {
+			for(int j = 0; j <= demo.L; j++) {
+				System.out.print(demo.cost_table[i][j].order + " ");
 			}
-			j += demo.gas_amounts[i];
+			System.out.println();
 		}
 	}
 }
