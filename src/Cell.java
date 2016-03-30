80fa5b704a04032d1a10ef4e548048db83f18217
GuiltyKing12 <bher@mines.edu>
2016-03-29 19:48:21 -0600

    PRAISE THE LORD WE FINISHED PROTESTANTS OGSSS

--- src/Cell.java
+++ src/Cell.java
@@ -1,6 +1,8 @@
 // cells that make up the cost table
 public class Cell {
-	public int cost;	// cost of reaching this cell
+	public int cost = 99999;	// cost of reaching this cell
 	boolean order = false;
-	public int parent;  // parent cell
+	public int parent_day;
+	public int parent_dayto;
+	public int current_L = -1;
 }
--- src/Demo.java
+++ src/Demo.java
@@ -1,65 +1,105 @@
 public class Demo {
-	int n = 8;
-	int L = 10;
-	Cell[][] cost_table = new Cell[n + 1][L + 1]; // 
-	int P = 5;
-	int c = 2;
-	int[] gas_amounts = {4, 2, 3, 5, 7, 9 ,2, 6};
-	int C = 1;
+	public final static int LEFT = 0;
+	public final static int UP = 1;
+	public final static int DIAG = 2;
 	
-	public void cost_calculate(int[] gas_amounts) {
-		int n = gas_amounts.length;
-		cost_table[0][0] = new Cell();
-		cost_table[0][0].cost = 0;
-				
-		for(int i = 0; i <= L; i ++) { 
-					cost_table[0][i] = new Cell();
-					cost_table[0][i].cost = 999999;
-		}
-
-		for(int j = 1; j <= n; j ++) {
-			System.out.print("Day " + j + ": ");
-			for(int k = 0; k <= L; k++) {
-				System.out.print(" " + k + " ");
-				cost_table[j][k] = new Cell();
-				cost_table[j][k].order = false;
-				cost_table[j][k].cost = 999999;
-				int day = 0;
-				for(int X = max(0, k + gas_amounts[day] - L); X <= k + gas_amounts[day]; X++) {
-					System.out.print("To " + (gas_amounts[day] + k) + " ");
-					int temp_cost = order(X) + C*(X) + cost_table[j - 1][k + gas_amounts[day] - X].cost + c * (k + gas_amounts[day] - X);
-					if(temp_cost < cost_table[j][k].cost) {
-						cost_table[j][k].cost = temp_cost;
-						cost_table[j][k].order = true;
-						System.out.println("here");
-					}
-				}
-				System.out.println();
-				day++;
+	public int n = 8;
+	public int L = 10;
+	public Cell[][] cost_table = new Cell[n + 1][n + 1]; 
+	public int P = 5;
+	public int c = 2;
+	public int[] opt = {LEFT, UP, DIAG};
+	public int[] gas_amounts = {1, 2, 3, 4, 5, 6, 7, 8};
+	public int C =   1;
+	
+	public Demo() {
+		for(int i = 0; i <= n; i++) {
+			for(int j = 0; j <= n; j++) {
+				cost_table[i][j] = new Cell();
 			}
 		}
 	}
 	
-	private int order(int x) {
-		// TODO Auto-generated method stub
-		if(x == 0) return 0;
-		else return P;
+	public void cost_calculate(int[] gas_amounts) {
+		cost_table[1][1].cost = P;
+		cost_table[1][1].parent_day = -1;
+		cost_table[1][1].parent_dayto = -1;
+		cost_table[1][1].order = true;
+		cost_table[1][1].current_L = gas_amounts[0];
+		for(int initial = 2; initial <= n; initial++) {
+			cost_table[1][initial].current_L = gas_amounts[initial - 1];
+			if(cost_table[1][initial].current_L + cost_table[1][initial - 1].current_L <= L) {
+				cost_table[1][initial].cost = cost_table[1][initial -1].cost + (initial-1) * c * gas_amounts[initial - 1];
+				cost_table[1][initial].order = true;
+				cost_table[1][initial].current_L += cost_table[1][initial -1].current_L;
+				cost_table[1][initial].parent_day = -1;
+				cost_table[1][initial].parent_dayto = -1;
+			}
+			else break;
+		}
+		
+		for(int day = 2; day <= n; day++) {
+			for(int dayto = day; dayto <= n; dayto++) {
+				if(day == dayto) cost_table[day][dayto].current_L = gas_amounts[dayto - 1];
+				else cost_table[day][dayto].current_L = cost_table[day][dayto - 1].current_L + gas_amounts[dayto -1];
+				
+				if(cost_table[day][dayto].current_L > L) continue;
+				
+				opt[LEFT] = P + cost_table[day][dayto - 1].cost;
+				opt[UP] = cost_table[day - 1][dayto].cost;
+				opt[DIAG] = P + cost_table[day - 1][dayto - 1].cost;
+				
+				cost_table[day][dayto].cost = opt[LEFT];
+				cost_table[day][dayto].parent_day = day;
+				cost_table[day][dayto].parent_dayto = dayto - 1;
+				cost_table[day][dayto].order = true;
+				
+				if(cost_table[day][dayto].cost > opt[UP]) {
+					cost_table[day][dayto].cost = opt[UP];
+					cost_table[day][dayto].parent_day = day - 1;
+					cost_table[day][dayto].parent_dayto = dayto;
+					cost_table[day][dayto].order = false;
+				}
+				if(cost_table[day][dayto].cost >= opt[DIAG]) {
+					cost_table[day][dayto].cost = opt[DIAG];
+					cost_table[day][dayto].parent_day = day - 1;
+					cost_table[day][dayto].parent_dayto = dayto - 1;
+					cost_table[day][dayto].order = true;
+				}
+			}
+		}
 	}
 
-	private int max(int i, int j) {
-		// TODO Auto-generated method stub
-		if( i > j) return i;
-		else return j;
+	public void traceback() {
+		Cell cell = new Cell();
+		cell = cost_table[n][n];
+		int day = n;
+		while(cell.parent_day != -1) {
+			if(cell.order) System.out.println("Ordered on day " + day);
+			day = cell.parent_day;
+			cell = cost_table[cell.parent_day][cell.parent_dayto];
+		}
+		System.out.println("Ordered on day 1");
 	}
-
-	public static void main(String[] args) {
-		Demo demo = new Demo();
-		demo.cost_calculate(demo.gas_amounts);
-		for(int i = 0; i <= demo.n; i++) {
-			for(int j = 0; j <= demo.L; j++) {
-				System.out.print(demo.cost_table[i][j].order + " ");
+	
+	public void print() {
+		for(int i = 1; i <= n; i++) {
+			for(int k = 1; k <= n; k++) {
+				if(k >= i) {
+					if(cost_table[i][k].cost != 99999) System.out.format("%4d", cost_table[i][k].cost);
+					else System.out.format("%4d", -1);
+				}
+				else System.out.format("%4d", -1);
 			}
 			System.out.println();
 		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		Demo demo = new Demo();
+		demo.cost_calculate(demo.gas_amounts);
+		demo.print();
+		demo.traceback();
 	}
 }
